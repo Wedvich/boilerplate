34cc6fdb36370601bffd73c46db5bb3fd249531a
Martin Wedvich <wedvich@gmail.com>
2019-04-18 15:44:39 +0200

    Enable CSS and hot loading

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+const entry = ['./src/index.tsx'];
+if (!isProduction) {
+  entry.unshift('react-hot-loader/patch');
+}
+
 module.exports = {
   mode: isProduction ? 'production' : 'development',
-  entry: './src/index.tsx',
+  entry,
   module: {
     rules: [
       {
         test: /\.(jsx?|tsx?)$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },

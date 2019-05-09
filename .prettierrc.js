3a59cea266061b939e0220c3adf15c891a367fa8
Martin Wedvich <martin.wedvich@makingwaves.com>
2019-05-09 22:19:53 +0200

    Add some tests

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  eslintIntegration: true,
   printWidth: 120,
   semi: true,
   singleQuote: true,
---
+++ test/components/App.test.ts
@@ -0,0 +1,5 @@
+import App from '../../src/components/app/App';
+
+describe('some thing', () => {
+  test('it succeeds', () => {});
+});

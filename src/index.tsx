bb58bebfc1dac9b2ea13299912d644fc4b3360d4
Martin Wedvich <wedvich@gmail.com>
2019-08-16 21:59:43 +0200

    Move app to root src dir

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
 
-import App from './components/app/App';
+import App from './App';
 
 render(<App />, document.getElementById('app-container'));
